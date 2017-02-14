81e3bbc4142f19a1b7e98559f962f884a27fe68d
Vinicius Mussato <vinicius.mussato@gmail.com>
2017-02-14 18:03:12 -0200

    first commit

---
+++ Frasedodia-app2/app/src/main/java/com/vmussato/frasedodia/MainActivity.java
@@ -0,0 +1,40 @@
+package com.vmussato.frasedodia;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textoNovaFrase;
+    private Button botaoNovaFrase;
+
+    private String[] frases = {"A primeira frase é chata",
+                               "a segunda é meio chata",
+                                "A terceira é muito chata",
+                                "E a última é boa",
+                                "asdjlsdjsakld ajdkljsdklasd asdsdsa adsadadasd"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textoNovaFrase = (TextView) findViewById(R.id.textoNovaFraseId);
+        botaoNovaFrase = (Button) findViewById(R.id.botaoNovaFraseId);
+
+        botaoNovaFrase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(frases.length);
+                textoNovaFrase.setText(frases[numeroAleatorio]);
+            }
+        });
+    }
+}
